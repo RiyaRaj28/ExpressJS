bb0826c142d801b4b4f3e161e0063f455868cfd1
Riya <Riya@itsme.com>
2024-01-01 13:30:18 +0530

    renamed views directory to templates

--- src/index.js
+++ src/index.js
@@ -8,18 +8,26 @@ const port = 8000;
 // console.log(path.join(__dirname, '../public')); 
 
 //makes you exit the src file and enter into the public folder!
-const staticPath = path.join(__dirname, '../public')
+const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname, "../templates");
 
 //to set the view engine
 app.set("view engine", "hbs");
+app.set("views", templatePath); 
 
-// app.use(express.static(staticPath));
-app.get("", (req, res) => {
-    res.render("index");
-});
+app.use(express.static(staticPath));
+
 
 // template engine route 
+app.get("/", (req, res) => {
+    res.render("index.hbs", {
+        name : "Riya", 
+    });
+});
 
+app.get("/about", (reqq,res) => {
+    res.render("about");
+})
 
 app.get("/", (req, res)=> {
     res.send("hello from express server");
